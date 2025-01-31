7fa823d6c8965254a3e03ee6998c5d0086dcfc5c
79631@alumnos.utleon.edu.mx <79631@alumnos.utleon.edu.mx>
2025-01-31 00:40:35 -0600

    Formularios

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+
 
 app = Flask(__name__)#objeto de la aplicacion creando la aplicacion
 
 @app.route('/')#decorador o ruta de la aplicacion
-def index(): 
-    return 'Hola Mundo'
+def index():
+    grupo='IDGS703'
+    lista=['Brayan','Jorge','Luis']
+    return render_template('index.html',grupo=grupo,lista=lista)
+
+
+@app.route('/OperasBas')#decorador o ruta de la aplicacion
+def Operas():
+    return render_template('OperasBas.html')
+
+@app.route('/resultado', methods=['POST', 'GET'])
+def resultado():
+    if request.method == 'POST':
+        num1 = request.form.get('n1')
+        num2 = request.form.get('n2')
+        resultado = int(num1) + int(num2)
+        return render_template('OperasBas.html', resultado=resultado)
+
+
+@app.route('/ejemplo1')#decorador o ruta de la aplicacion
+def ejemplo1():
+    return render_template('ejemplo1.html')
+
+@app.route('/ejemplo2')#decorador o ruta de la aplicacion
+def ejemplo2():
+    return render_template('ejemplo2.html')
+
+
+
+
 
 
 @app.route('/hola')#decorador o ruta de la aplicacion
@@ -36,10 +66,29 @@ def suma(n1,n2):
     return 'La suma es: {}!!!'.format(n1+n2)
 
 
-@app.route('/default')
-@app.route('/default/<string:nom>')
-def func(nom='Brayan'): 
-    return 'El nombre de nom es: ' + nom
+# @app.route('/default')
+# @app.route('/default/<string:nom>')
+# def func(nom='Brayan'): 
+#     return 'El nombre de nom es: ' + nom
+
+
+
+@app.route('/form1')
+def form1():
+    return '''
+        <form>
+        <label>Nombre:</label>
+        <input type="text" name="nombre" placeholder="Escribe tu nombre">
+        </br>
+        <label>Nombre:</label>
+        <input type="text" name="nombre" placeholder="Escribe tu nombre">
+        </br>
+        <label>Nombre:</label>
+        <input type="text" name="nombre" placeholder="Escribe tu nombre">
+        </br>
+    '''
+
+
 
 
 
